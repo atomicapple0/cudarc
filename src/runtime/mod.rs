effe377bd8fdca1a1694ab65a252808932b9a39c
atomicapple0 <brianzhangaa@gmail.com>
2023-11-27 22:22:05 -0500

    link cudart properly and add test

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,3 +1,32 @@
 pub mod result;
 #[allow(warnings)]
 pub mod sys;
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        driver::sys::CUresult,
+        runtime::sys::{cudaDriverGetVersion, cudaError},
+    };
+
+    // This should work without GPU
+    #[test]
+    fn get_version() {
+        let mut version: i32 = 0;
+        let result = unsafe { cudaDriverGetVersion(&mut version as *mut i32) };
+        if result != cudaError::cudaSuccess {
+            panic!("Cannot get driver version: ERROR={:?}", result);
+        }
+        println!("Version = {}", version);
+    }
+
+    #[test]
+    fn into_result() {
+        let result = unsafe {
+            let mut version = 0;
+            cudaDriverGetVersion(&mut version as _)
+        };
+        let result: CUresult = result.into();
+        assert!(result.result().is_ok());
+    }
+}
