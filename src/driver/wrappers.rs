c276a1c265a9a048bc9e13f19bb0289916c12c31
atomicapple0 <brianzhangaa@gmail.com>
2023-11-28 00:21:14 -0500

    fix void wrapper

--- src/driver/wrappers.rs
+++ src/driver/wrappers.rs
@@ -3,22 +3,22 @@ use libc::c_void;
 
 use super::sys::{CUdevice, CUdeviceptr, CUevent, CUkernel, CUlibrary, CUmodule, CUstream};
 
-macro_rules! sync_send {
-    ($wrapper: ident, $type: ident) => {
-        #[derive(Debug)]
+macro_rules! impl_wrapper {
+    ($wrapper: ident, $type: ty) => {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
         pub struct $wrapper(pub $type);
         unsafe impl Send for $wrapper {}
         unsafe impl Sync for $wrapper {}
     };
 }
 
-sync_send!(Void, c_void);
-sync_send!(WrCUcontext, CUcontext);
-sync_send!(WrCUfunction, CUfunction);
-sync_send!(WrCUmodule, CUmodule);
-sync_send!(WrCUstream, CUstream);
-sync_send!(WrCUdevice, CUdevice);
-sync_send!(WrClibrary, CUlibrary);
-sync_send!(WrCUkernel, CUkernel);
-sync_send!(WrCUdeviceptr, CUdeviceptr);
-sync_send!(WrCUevent, CUevent);
+impl_wrapper!(Void, *const c_void);
+impl_wrapper!(WrCUcontext, CUcontext);
+impl_wrapper!(WrCUfunction, CUfunction);
+impl_wrapper!(WrCUmodule, CUmodule);
+impl_wrapper!(WrCUstream, CUstream);
+impl_wrapper!(WrCUdevice, CUdevice);
+impl_wrapper!(WrClibrary, CUlibrary);
+impl_wrapper!(WrCUkernel, CUkernel);
+impl_wrapper!(WrCUdeviceptr, CUdeviceptr);
+impl_wrapper!(WrCUevent, CUevent);
